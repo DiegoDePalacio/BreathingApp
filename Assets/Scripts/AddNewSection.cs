8686b85946fb405762eb899f26ee7770c26bec73
Diego <diegodepalacio@gmail.com>
2022-10-08 11:41:37 +0200

    Add the possibility for multiple users at the same time. Changed the base font. Added 8 colors for different users.

---
+++ Assets/Scripts/AddNewSection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BreathingApp
+{
+    [RequireComponent(typeof(Button))]
+    public class AddNewSection : MonoBehaviour
+    {
+        [SerializeField] private LayoutGroup _layoutGroup;
+        [SerializeField] private Breathe _breatheSection;
+
+        private Button _button;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(_layoutGroup);
+            Assert.IsNotNull(_breatheSection);
+            
+            _button = GetComponent<Button>();
+            Assert.IsNotNull(_button);
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnAddNewSectionButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnAddNewSectionButtonClicked);
+        }
+
+        private void OnAddNewSectionButtonClicked()
+        {
+            Instantiate(_breatheSection, _breatheSection.transform.parent);
+        }
+    }
+}
---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace BreathingApp
+{
+    [RequireComponent(typeof(RawImage))]
+    public class ColorPicker : MonoBehaviour
+    {
+        [SerializeField] private List<Color> _colors;
+
+        private RawImage _image;
+
+        private void Awake()
+        {
+            _image = GetComponent<RawImage>();
+            Assert.IsNotNull(_image);
+        }
+
+        public void OnEnable()
+        {
+            Recolor();
+        }
+
+        public void Recolor()
+        {
+            int childNumber = transform.GetSiblingIndex();
+            int colorIndex = childNumber % _colors.Count;
+            _image.color = _colors[colorIndex];
+        }
+    }
+}
